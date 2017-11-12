0fa0a4d7b386d5d5cc1a8ce5c45a806af8b5d7cf
David Nekovář <nekovard@gmail.com>
2017-11-12 17:31:52 +0100

    adding mobile nav
    not initialized with js, so not working but its there...

--- src/components/header.js
+++ src/components/header.js
@@ -1,27 +1,27 @@
 import React, { Component } from 'react';
 
 class Header extends Component {
+    renderMenu (menuClass, menuId) {
+        return (
+        <ul className={menuClass} id={menuId}>
+            <li><a href="/new-test">New test</a> </li>
+            <li><a href="/saved-tests">Saved tests</a></li>
+            <li><a href="/settings"><i className="material-icons">settings</i></a></li>
+            <li><a className="waves-effect waves-light btn">Account<i className="left material-icons">account_circle</i></a></li>
+        </ul>
+        );
+    }
+    
     render() {
         return (
         <div> 
             <header className="header">
                 <nav>
                     <div className="nav-wrapper">
-                        <a href="/" className="brand-logo left">Logo</a>
-                        <ul className="right">
-                            <li>
-                                <a href="/new-test">New test</a>
-                            </li>
-                            <li>
-                                <a href="/saved-tests">Saved tests</a>
-                            </li>
-                            <li>
-                                <a href="/settings">
-                                    <i className="material-icons">settings</i>
-                                </a>
-                            </li>
-                            <li><a className="waves-effect waves-light btn">Account<i className="left material-icons">account_circle</i></a></li>
-                        </ul>
+                        <a href="/" className="brand-logo">Logo</a>
+                        <a href="#" data-activates="mobile-menu" className="button-collapse"><i className="material-icons">menu</i></a>
+                            {this.renderMenu("right hide-on-med-and-down")}
+                            {this.renderMenu("side-nav", "mobile-menu")}
                     </div>
                 </nav>
             </header>                       
