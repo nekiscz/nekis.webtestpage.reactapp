3c4387185c2c003d4997ddcbc9efaea0d3cec381
David Nekovář <nekovard@gmail.com>
2017-11-12 21:47:05 +0100

    text fiedl component

---
+++ src/components/text_field.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const TextField = (field) => {
+    var error = '';
+    var wrapperClass = `input-field ${field.wrapperClass ? field.wrapperClass : ''}`;
+
+    if (field.meta.error) { 
+        error = displayError(field.meta.error) 
+    }
+    
+    return (
+    <div className={wrapperClass}>
+        <input 
+            {...field.input}
+            placeholder={field.placeholder} 
+            id={field.name} 
+            type="text"/>
+        <label htmlFor={field.name}>{field.label}{field.meta.touched ? error : ''}</label>
+    </div>
+    )
+}
+
+const displayError = (text) => {
+    return (
+        <span className='new badge red accent-2' data-badge-caption={text} />
+    )
+}    
+
+export default TextField;
